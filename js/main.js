7d024544aa10b05fa2d6c070cde92d61296ba0d4
hedgeinthehog <37834796+hedgeinthehog@users.noreply.github.com>
2021-01-30 19:04:05 +0200

    adds js creation and rendering of gallery's markup

---
+++ js/main.js
@@ -0,0 +1,9 @@
+import galleryItems from "./gallery-items.js";
+
+const galleryRef = document.querySelector('.js-gallery');
+
+const galleryMarkup = galleryItems.reduce((gallery, item) => 
+	gallery + `<li class="gallery__item"><a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/></a></li>`, ''
+);
+
+galleryRef.insertAdjacentHTML('afterbegin', galleryMarkup);
